bb98bfc9f1d7d3930d532f3473b1f242fc1db703
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-09-16 00:52:25 +0100

    Demo HomePage Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+import Header from "@/components/Header"
+import Screen from "@/components/Screen"
+import LinksForm from "@/components/LinksForm"
+
 export default function Home() {
   return (
-    <main className="">
-      Links Page Home
+    <main className="bg-cgray mx-3 ">
+      <Header/>
+      <div className="flex gap-x-3 mt-5">
+        <Screen/>
+        <LinksForm/>
+      </div>
     </main>
   )
 }
