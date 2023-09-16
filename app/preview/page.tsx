8e43e696635539565bfeb71b713d31d480aee71f
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-09-16 16:14:41 +0100

    Profile Picture card added

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,17 +1,28 @@
+'use client'
+
+import { useGlobalContext } from "@/context/AppContext"
 import HeaderPreview from "@/components/HeaderPreview"
 import { AiOutlineArrowRight } from "react-icons/ai"
 import {BsGithub} from "react-icons/bs"
 import {AiFillYoutube, AiFillLinkedin} from "react-icons/ai"
-export default function Page() {
+
+export default function Page() {    
     return (
         <>
             <div className="bg-cpurple h-56 rounded-b-3xl">
                 <HeaderPreview />
                 <div className="flex justify-center relative top-20">
                     <div className=" h-[360px] w-56 block bg-white rounded-xl shadow-xl">
-                        <div className="mx-auto h-24 w-24 bg-white rounded-full relative top-3 border-[4px] border-cpurple"></div>
-                        <p className="font-bold text-2xl text-center mt-4">Ben Wright</p>
-                        <p className="font-base text-xs text-gray-500 text-center mt-2">ben@example.com</p>
+                        <div 
+                            className="mx-auto h-24 w-24 bg-white rounded-full relative top-3 border-[4px] border-cpurple bg-transparent bg-cover shadow-lg bg-center bg-no-repeat"
+                            style={{
+                                backgroundImage: `url('${localStorage.profilePicture}')`,
+                            }}
+                        >
+
+                        </div>
+                        <p className="font-bold text-2xl text-center mt-4">{localStorage.name.length === 0 ? 'First Name' : localStorage.name } {localStorage.lastname.length === 0 ? 'Last Name' : localStorage.lastname}</p>
+                        <p className="font-base text-xs text-gray-500 text-center mt-2">{ localStorage.email.length === 0 ? 'Email Address' : localStorage.email }</p>
                         <div className="cursor-pointer mx-auto bg-github w-48 p-2 mt-10 text-sm text-white rounded-md flex flex-row justify-between items-center">
                             <div>
                                 <BsGithub className="inline-block mr-1"/>
