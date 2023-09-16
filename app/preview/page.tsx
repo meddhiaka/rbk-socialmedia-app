ddf71435a5d6900b5b2fb107afdd8e466888930d
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-09-16 03:02:08 +0100

    Preview Route Added

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,5 +1,15 @@
+import HeaderPreview from "@/components/HeaderPreview"
+
 export default function Page() {
     return (
-        <div></div>
+        <>
+            <div className="bg-cpurple h-96 rounded-b-3xl">
+                <HeaderPreview />
+                <div className="flex justify-center relative top-36">
+                    <div className=" h-96 w-56 block bg-white rounded-xl shadow-xl"></div>
+                </div>
+            </div>
+            
+        </>
     )
 }
