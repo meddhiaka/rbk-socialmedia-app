ffe6deda46840ffd5cef585ebb1bb33ae12f519e
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-09-16 16:14:14 +0100

    Notification Container Created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GlobalContextProvider } from '@/context/AppContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +21,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalContextProvider>{children}</GlobalContextProvider>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={3000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable={false}
+          pauseOnHover
+          theme="dark"
+        />
+      </body>
     </html>
   )
 }
